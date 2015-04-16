7097b6a330369ed92a58119fd9a46027ff316147
pursertim <timpurser@Tims-MBP.uwe.ac.uk>
2015-04-16 10:05:03 +0100

    What's Left Project
    Creative Technology Project

---
+++ ParseTutorial/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewController.swift
+//  ParseTutorial
+//
+//  Created by Tim Purser on 15/04/2015.
+//  Copyright (c) 2015 Tim Purser. All rights reserved.
+//  reference Bizzi Bradbury medium.com/@BizziBradbury
+
+//
+
+import UIKit
+
+class CustomTableViewCell: PFTableViewCell {
+	
+	@IBOutlet weak var customNameEnglish: UILabel!
+	@IBOutlet weak var customCapital: UILabel!
+	@IBOutlet weak var customFlag: PFImageView!
+	
+}
